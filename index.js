c20e16f4b8d80dbcc2b297130ce67313fcd1400c
khairul-islam98 <janeyrahman900@gmail.com>
2023-10-18 20:47:11 +0600

    brandDB update

--- index.js
+++ index.js
@@ -28,12 +28,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const brandCollection = client.db('brandDB').collection('brand')
+
+    app.get('/brand', async(req, res) => {
+      const result = await brandCollection.find().toArray()
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
